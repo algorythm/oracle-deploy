e073b6bf54e87d1e0124c45772c760ce9b1d3ba8
Anders W. Olsen <anders@wiberg.tech>
2017-04-12 21:14:40 +0200

    Better structure

--- includes/main.sh
+++ includes/main.sh
@@ -13,19 +13,19 @@ nmcli g hostname $fqdn
 nmcli con mod eth0 ipv4.addr $external ipv4.gateway 10.174.234.1
 nmcli con mod eth1 ipv4.addr $internal
 
-echo "Network has been changed. Starting weberp12..."
-systemctl restart network
-echo "weberp12 has been started."
-
-echo "Defining Virtual hosts in WLS..."
-# Some more code here
+echo "Network has been changed."
 
 echo;
 
+echo "Changing hosts file..."
 ./includes/change_hosts.sh "$fqdn" "$ip"
+echo "Changing formsweb.cfg..."
 ./includes/change_formsweb.sh "$fqdn"
+echo "Setting up virtual host in WLS..."
 ./includes/change_vhost_wls.sh "$fqdn"
+echo "Configuring ORDS..."
 ./includes/configure_ords.sh
+echo "Creating virtual hosts for apache..."
 ./includes/httpd_add_virtual_host.sh "$fqdn"
 
 # Enable the weberp12 service so it starts when the system boots
