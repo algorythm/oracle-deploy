36fdf792ae07a98caee5528784abe0056de29d14
Anders W. Olsen <anders@wiberg.tech>
2017-04-19 13:56:55 +0200

    Adding possibility to reconfigure ords

--- includes/main.sh
+++ includes/main.sh
@@ -9,9 +9,9 @@ echo " q - exit"
 while true; do
     read -p "Choice: " choice
     case $choice in
-        [1]* ) ./includes/full-config.sh break;;
-		[2]* ) ./includes/ords.sh break;;
-		[3]* ) echo "Not implemented yet...";;
+        [1]* ) ./includes/full-config.sh; break;;
+		[2]* ) ./includes/ords.sh; break;;
+		[3]* ) ./includes/reconfigure_ords.sh; break;;
 		[q]* ) echo "Exiting..."; break;;
         * ) echo "Select one of the possibilities!";
     esac
