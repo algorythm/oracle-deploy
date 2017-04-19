6554f219fc58ec90741e72bd55b02ae38fa2e35f
Anders W. Olsen <anders@wiberg.tech>
2017-04-19 12:23:21 +0200

    testing to add scripts for full / semi config

--- includes/main.sh
+++ includes/main.sh
@@ -2,15 +2,15 @@
 echo "Choose one of the following possibilities:"
 echo " 1 - full configuration"
 echo " 2 - only ords configuration"
-echo " 3 - reconfigure ords"
+echo " 3 - reconfigure ords (test)"
 
 
 while true; do
     read -p "Choice: " choice
     case $choice in
-        [1]* ) echo "full config"; break;;
-		[2]* ) echo "ords config"; break;;
-		[3]* ) echo "reconfgire ords"; break;;
+        [1]* ) echo ./includes/full-config.sh break;;
+		[2]* ) echo ./includes/ords.sh break;;
+		[3]* ) echo ./includes/test.sh break;;
         * ) echo "Select one of the possibilities!";
     esac
 done
