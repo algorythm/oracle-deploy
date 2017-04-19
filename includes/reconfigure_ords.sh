92133bfdef96490e821fc0544fa3e8b98bf49da7
Anders W. Olsen <anders@wiberg.tech>
2017-04-19 13:55:25 +0200

    Corrected directory name

--- includes/reconfigure_ords.sh
+++ includes/reconfigure_ords.sh
@@ -2,7 +2,7 @@
 echo "Removing old ords files"
 rm -rf /usr/share/tomcat/webapps/ords*
 cd /home/tomcat/ordstomcat
-rm -rf docs logs ordstomcatconf params examples ords.war readme.html
+rm -rf docs logs tomcatconfig params examples ords.war readme.html
 unzip ords.*.zip
 
 ./includes/configure_ords.sh
