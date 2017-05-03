c3e415469a1c81dd63acd9baed1d2865b4c3a8ef
Anders W. Olsen <anders@wiberg.tech>
2017-05-03 12:56:32 +0200

    fixing path to oracle hoem

--- includes/configure_nodemanager.sh
+++ includes/configure_nodemanager.sh
@@ -1,4 +1,4 @@
-su - oracle $ORACLE_HOME/oracle_common/common/bin/wlst.sh << EOF > /tmp/nodemanager.log
+su - oracle /home/oracle/Oracle/Middleware12c/ofr1/oracle_common/common/bin/wlst.sh << EOF > /tmp/nodemanager.log
 nmConnect('sberpsaas','sbs10ias','localhost','5556','SBErpc12','/home/oracle/Oracle/Middleware12c/ofr1/user_projects_domains/SBErpc12')
 prps = makePropertiesObject("AdminURL=http://ottos12.softbase.ch;7001;Username=sberpsaas;Password=sbs10ias;weblogic.ListenPort=7001")
 nmStart("AdminServer", props=prps)
