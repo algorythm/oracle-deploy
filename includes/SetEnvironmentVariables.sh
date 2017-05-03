a4dfa12d23332f62edce9ffe9f653e5ff99d862b
Anders W. Olsen <anders@wiberg.tech>
2017-05-03 12:45:46 +0200

    Fixing spelling mistake

--- includes/SetEnvironmentVariables.sh
+++ includes/SetEnvironmentVariables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export ORACLE_INSTALL_USER=oracle
 export ORACLE_INSTALL_GROUP=oinstall
-export DOMAIN_CONFIGURATION_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user    _projects/domains/SBErpc12
-export NODE_MANAGER_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user_project    s/domains/SBErpc12/nodemanager
+export DOMAIN_CONFIGURATION_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12
+export NODE_MANAGER_HOME=/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12/nodemanager
