6ee2c200e2d172e141a87833863ff5945cf9e874
Anders W. Olsen <anders@wiberg.tech>
2017-05-03 12:58:55 +0200

    Fixing path to oracle domain

--- includes/configure_nodemanager.sh
+++ includes/configure_nodemanager.sh
@@ -1,5 +1,5 @@
 su - oracle /home/oracle/Oracle/Middleware12c/ofr1/oracle_common/common/bin/wlst.sh << EOF > /tmp/nodemanager.log
-nmConnect('sberpsaas','sbs10ias','localhost','5556','SBErpc12','/home/oracle/Oracle/Middleware12c/ofr1/user_projects_domains/SBErpc12')
+nmConnect('sberpsaas','sbs10ias','localhost','5556','SBErpc12','/home/oracle/Oracle/Middleware12c/ofr1/user_projects/domains/SBErpc12')
 prps = makePropertiesObject("AdminURL=http://ottos12.softbase.ch;7001;Username=sberpsaas;Password=sbs10ias;weblogic.ListenPort=7001")
 nmStart("AdminServer", props=prps)
 exit()
